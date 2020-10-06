8bced7bdb8688411ccf42c7211c619ab916ed9af
dpuskuri <64773180+dpuskuri@users.noreply.github.com>
2020-10-06 13:01:55 +0200

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,26 @@
+ 1  apt-get update
+    2  systemctl status docker
+    3  systemctl status Docker
+    4  sudo systemctl status Docker
+    5  sudo systemctl daemon-reload
+    6  sudo systemctl start Docker
+    7  curl -sSL https://get.docker.com/ | sh
+    8  docker login --username=yourhubusername --email=youremail@company.com
+    9  docker login --username=deepakrao2323 --email=dpuskuri@gmail.com
+   10  docker login --username=deepakrao2323
+   11  systemctl status docker
+   12  docker pull hello-world
+   13  docker images
+   14  docker --version
+   15  vi /etc/ssh/sshd_config
+   16  useradd deepak
+   17  mkdir /home/deepak
+   18  vi /etc/passwd
+   19  ls -ld /home/deepak
+   20  chown deepak:deepak /home/deepak
+   21  su -deepak
+   22  su - deepak
+   23  vi /etc/group
+   24  passwd deepak
+   25  su - deepak
+   26  history
